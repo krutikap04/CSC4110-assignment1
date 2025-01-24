7766ea6053a98b7ea95c8194d29b753beffb62bc
Krutika Patil <krutika@Krutikas-MacBook-Air.local>
2025-01-24 16:41:13 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Krutika Patil <krutikapatil125@gmail.com>
 #include<iostream>  
   
 using namespace std; 
